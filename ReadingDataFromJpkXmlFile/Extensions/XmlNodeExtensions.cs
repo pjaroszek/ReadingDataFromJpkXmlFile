d96cd35a3da93a7e667d14391289a250a8b38517
Pawel Jaroszek <pjaroszek@gmail.com>
2019-12-27 21:30:19 +0100

    added method to the validate and read xml file

---
+++ ReadingDataFromJpkXmlFile/Extensions/XmlNodeExtensions.cs
@@ -0,0 +1,58 @@
+﻿namespace Jaroszek.Proof.Of.Concept.ReadingDataFromJpkXmlFile.Extensions
+{
+    using System;
+    using System.Globalization;
+    using System.Xml;
+
+    public static class XmlNodeExtensions
+    {
+        public static DateTime? ToDateTime(this XmlNode source)
+        {
+            if (source is null)
+            {
+                return null;
+            }
+
+            return DateTime.Parse(source.InnerText);
+        }
+
+        public static string ToText(this XmlNode source)
+        {
+            if (source is null)
+            {
+                return null;
+            }
+
+            return source.InnerText;
+        }
+
+        public static decimal? ToDecimal(this XmlNode source) // var cultureInfo = new CultureInfo("pl-PL");
+        {
+            if (source is null)
+            {
+                return null;
+            }
+
+            var text = source?.InnerText;
+
+            if (decimal.TryParse(text, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out var value))
+            {
+                return value;
+            }
+
+            throw new ArgumentOutOfRangeException();
+        }
+
+        public static dynamic toList(this XmlNodeList source)
+        {
+
+            foreach (XmlNode xn in source)
+            {
+                string firstName = xn["tns:NrKontrahenta"].InnerText;
+            }
+
+            return null;
+        }
+
+    }
+}
--- ReadingDataFromJpkXmlFile/Program.cs
+++ ReadingDataFromJpkXmlFile/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace Jaroszek.Proof.Of.Concept.ReadingDataFromJpkXmlFile
+﻿using System;
+using Jaroszek.Proof.Of.Concept.ReadingDataFromJpkXmlFile.Services;
+
+namespace Jaroszek.Proof.Of.Concept.ReadingDataFromJpkXmlFile
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ReadingDataFromJpkFile readingDataFromJpk = new ReadingDataFromJpkFile();
+
+            string file = @"C:\Users\Pawel\Desktop\JPK\JPK_VAT_20180101_20180131_f91060.xml";
+            readingDataFromJpk.ReadFile(file);
+            Console.ReadLine();
+
         }
     }
 }
